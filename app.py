365097f8e005095ef05e5ba7c589afc458913c23
Truc Tran <tracytrle@gmail.com>
2024-07-03 23:14:41 -0500

    done @me

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from config import ApplicationConfig
 from models import db, User
 from flask_bcrypt import Bcrypt
 from flask import abort
+from flask_session import Session
 
 
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 bcrypt = Bcrypt(app)
+server_session = Session(app)
 db.init_app(app)
 
 with app.app_context():
@@ -17,6 +19,20 @@ with app.app_context():
 @app.route('/')
 def index():
   return "Hello, World!"
+
+#return current loggin user infor
+@app.route('/@me')
+def get_current_user():
+    user_id = session.get('user_id')
+    
+    if not user_id:
+        return jsonify({"error": "Unauthorized"}), 401
+
+    user = User.query.filter_by(id=user_id).first()    
+    return jsonify({
+        "id": user.id,
+        "email": user.email
+    })
 
 
 @app.route('/register', methods=["POST"]) # This route will be used to register a new user.
@@ -50,6 +66,7 @@ def login_user():
     if not bcrypt.check_password_hash(user.password, password):
         return jsonify({"error": "Unauthorized"}), 401
     
+    session['user_id'] = user.id
     return jsonify({
         "id": user.id,
         "email": user.email
--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import redis
 
 load_dotenv() # take environment variables from .env.
 
@@ -8,6 +9,10 @@ class ApplicationConfig:
     SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO= True
-
+    SESSION_TYPE = 'redis'
+    SESSION_PERMANENT = False
+    SESSION_USE_SIGNER = True
+    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    
     # This sets the SQLALCHEMY_DATABASE_URI attribute to the string sqlite:///site.db. This URI is used by SQLAlchemy, the database toolkit for Flask, to specify the location of the SQLite database file named site.db.
     # This sets the SQLALCHEMY_TRACK_MODIFICATIONS attribute to False. This configuration option disables the modification tracking feature of SQLAlchemy, which can help improve performance.
