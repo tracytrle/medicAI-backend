4ba7c5df1198ca2819e3239d81540b578b1b12be
Truc Tran <tracytrle@gmail.com>
2024-07-03 22:24:25 -0500

    working on login

--- app.py
+++ app.py
@@ -38,6 +38,24 @@ def register_user():
         "email": new_user.email
     })
 
+@app.route('/login', methods=["POST"]) # This route will be used to log in an existing user.
+def login_user():
+    email = request.json['email']
+    password = request.json['password']
+
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    return jsonify({
+        "id": user.id,
+        "email": user.email
+    })
+
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
